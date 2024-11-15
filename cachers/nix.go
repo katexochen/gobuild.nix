2292a2c6d1149fc4c0cf811bee6dcef8626855ec
adisbladis <adisbladis@gmail.com>
2024-11-15 07:58:02 +0000

    go-tool-cache/go-cacher: fork into gobuild-nix-cacher

--- cachers/nix.go
+++ cachers/nix.go
@@ -10,62 +10,80 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"time"
+	"sync"
 )
 
 // indexEntry is the metadata that DiskCache stores on disk for an ActionID.
 type indexEntry struct {
-	Version   int    `json:"v"`
 	OutputID  string `json:"o"`
 	Size      int64  `json:"n"`
 	TimeNanos int64  `json:"t"`
 }
 
 type DiskCache struct {
-	Dir     string
+	// Cache input directories.
+	// Normally from NIX_GOCACHE
+	InputDirs []string
+
+	// Cache output directory path.
+	OutDir string
+
+	// Timestamp to store put requests with.
+	// Normally derived from SOURCE_DATE_EPOCH.
+	TimeNanos int64
+
+	// Debug cache hits/misses
 	Verbose bool
+
+	// Wait for in flight writes to finish on shutdown
+	wg sync.WaitGroup
 }
 
 func (dc *DiskCache) Get(ctx context.Context, actionID string) (outputID, diskPath string, err error) {
-	actionFile := filepath.Join(dc.Dir, fmt.Sprintf("a-%s", actionID))
-	ij, err := os.ReadFile(actionFile)
-	if err != nil {
-		if os.IsNotExist(err) {
-			err = nil
-			if dc.Verbose {
-				log.Printf("disk miss: %v", actionID)
-			}
-		}
-		return "", "", err
-	}
-	var ie indexEntry
-	if err := json.Unmarshal(ij, &ie); err != nil {
-		log.Printf("Warning: JSON error for action %q: %v", actionID, err)
-		return "", "", nil
-	}
-	if _, err := hex.DecodeString(ie.OutputID); err != nil {
-		// Protect against malicious non-hex OutputID on disk
-		return "", "", nil
-	}
-	return ie.OutputID, filepath.Join(dc.Dir, fmt.Sprintf("o-%v", ie.OutputID)), nil
-}
+	filename := fmt.Sprintf("a-%s", actionID)
 
-func (dc *DiskCache) OutputFilename(objectID string) string {
-	if len(objectID) < 4 || len(objectID) > 1000 {
-		return ""
-	}
-	for i := range objectID {
-		b := objectID[i]
-		if b >= '0' && b <= '9' || b >= 'a' && b <= 'f' {
-			continue
+	for _, dir := range dc.InputDirs {
+		actionFile := filepath.Join(dir, filename)
+
+		ij, err := os.ReadFile(actionFile)
+		if err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+
+			return "", "", nil
 		}
-		return ""
+
+		var ie indexEntry
+		if err := json.Unmarshal(ij, &ie); err != nil {
+			log.Printf("Warning: JSON error for action %q: %v", actionID, err)
+			return "", "", nil
+		}
+
+		if _, err := hex.DecodeString(ie.OutputID); err != nil {
+			// Protect against malicious non-hex OutputID on disk
+			return "", "", nil
+		}
+
+		return ie.OutputID, filepath.Join(dir, fmt.Sprintf("o-%v", ie.OutputID)), nil
 	}
-	return filepath.Join(dc.Dir, fmt.Sprintf("o-%s", objectID))
+
+	if dc.Verbose {
+		log.Printf("disk miss: %v", actionID)
+	}
+
+	return "", "", nil
 }
 
 func (dc *DiskCache) Put(ctx context.Context, actionID, objectID string, size int64, body io.Reader) (diskPath string, _ error) {
-	file := filepath.Join(dc.Dir, fmt.Sprintf("o-%s", objectID))
+	dc.wg.Add(1)
+	defer dc.wg.Done()
+
+	if dc.OutDir == "" {
+		return "", fmt.Errorf("received put but no output directory was set")
+	}
+
+	file := filepath.Join(dc.OutDir, fmt.Sprintf("o-%s", objectID))
 
 	// Special case empty files; they're both common and easier to do race-free.
 	if size == 0 {
@@ -85,19 +103,24 @@ func (dc *DiskCache) Put(ctx context.Context, actionID, objectID string, size in
 	}
 
 	ij, err := json.Marshal(indexEntry{
-		Version:   1,
 		OutputID:  objectID,
 		Size:      size,
-		TimeNanos: time.Now().UnixNano(),
+		TimeNanos: dc.TimeNanos,
 	})
 	if err != nil {
 		return "", err
 	}
-	actionFile := filepath.Join(dc.Dir, fmt.Sprintf("a-%s", actionID))
+
+	actionFile := filepath.Join(dc.OutDir, fmt.Sprintf("a-%s", actionID))
 	if _, err := writeAtomic(actionFile, bytes.NewReader(ij)); err != nil {
 		return "", err
 	}
+
 	return file, nil
+}
+
+func (dc *DiskCache) Wait() {
+	dc.wg.Wait()
 }
 
 func writeAtomic(dest string, r io.Reader) (int64, error) {
