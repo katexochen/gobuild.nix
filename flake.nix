ac5debe3736308c8d6a6729ab95926e7790f2d09
Paul Meyer <katexochen0@gmail.com>
2025-01-03 09:54:33 +0100

    setup garnix
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- flake.nix
+++ flake.nix
@@ -32,5 +32,26 @@
         }
       );
 
+      # Exposed for garnix CI.
+      # Currently, dots are not allowed in package names,
+      # see https://github.com/garnix-io/issues/issues/104.
+      # Also builds of legacyPackages are not supported yet,
+      # see https://github.com/garnix-io/issues/issues/92.
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          filterDerivations = pkgs.lib.filterAttrs (n: v: pkgs.lib.isDerivation v);
+          prefixNameWith = prefix: pkgs.lib.mapAttrs' (n: v: pkgs.lib.nameValuePair (prefix + n) v);
+          removeDotsFromName = pkgs.lib.mapAttrs' (
+            n: v: pkgs.lib.nameValuePair (lib.replaceStrings [ "." ] [ "-" ] n) v
+          );
+        in
+        prefixNameWith "goPackages-" (
+          removeDotsFromName (filterDerivations self.legacyPackages.${system}.goPackages)
+        )
+        // (filterDerivations self.legacyPackages.${system})
+
+      );
     };
 }
