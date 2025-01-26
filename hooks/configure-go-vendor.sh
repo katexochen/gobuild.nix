a5130b8addee3b39506069ef21284e0a264bfbc3
Paul Meyer <katexochen0@gmail.com>
2025-01-26 10:27:31 +0100

    hooks.configureGoVendor: rewrite go.mod
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- hooks/configure-go-vendor.sh
+++ hooks/configure-go-vendor.sh
@@ -3,11 +3,24 @@ echo "Sourcing configure-go-vendor-hook"
 goConfigureVendor() {
   echo "Executing goConfigureVendor"
 
-  echo "NIX_GO_VENDOR: ${NIX_GO_VENDOR}"
-  if [ -z "${NIX_GO_VENDOR}" ]; then
+  if [ -z "${NIX_GO_VENDOR:-}" ]; then
     echo "NIX_GO_VENDOR is not set, not configuring Go vendor"
     return
   fi
+  echo "NIX_GO_VENDOR: ${NIX_GO_VENDOR:-}"
+
+  # Cleanup vendor, we want to write our own vendor dir.
+  rm -rf vendor
+  # Cleanup go.sum, sums won't match with the rewritten go.mod.
+  rm -f go.sum
+  # Keep backup of go.mod for debugging.
+  mv go.mod go.mod.old
+  # Use module directive from old go.mod.
+  grep -E '^module [^ ]+$' go.mod.old > go.mod
+  # Add go directive to go.mod.
+  echo "go @go_version@" >> go.mod
+
+  export HOME=$(mktemp -d)
 
   for dep in ${NIX_GO_VENDOR}; do
     # Input form is <pname>@v<version>:<storepath>
@@ -18,12 +31,18 @@ goConfigureVendor() {
 
     echo "adding ${pname}@${version} to vendor, storepath: ${storepath}"
 
+    # pname is a path, the last element in this path should be our symlink.
+    # Create the other directories from pname.
     mkdir -p "vendor/${pname%/*}"
-    ln -s "${storepath}" vendor/${pname}
+    cp -r "${storepath}" vendor/${pname} # TODO: use ln -s, special case golang.org/x
+
+    # Add the dependency to go.mod.
+    echo "adding ${pname}@${version} to go.mod"
+    echo "require ${pname} ${version}" >> go.mod
   done
 
-  echo "Updating go directive in go.mod to @go_version@"
-  sed -E -i 's/^go [0-9]+\.[0-9]+(\.[0-9]+)?/go @go_version@/' go.mod
+  echo "go.mod after rewrite:"
+  cat go.mod
 
   echo "Finished executing goConfigureVendor"
 }
