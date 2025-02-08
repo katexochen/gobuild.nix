fa5a7114a4ecab36b4ee996a566195e53c7c564d
Paul Meyer <katexochen0@gmail.com>
2025-02-08 12:25:27 +0100

    build keep-sorted
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- package.nix
+++ package.nix
@@ -66,4 +66,45 @@ in
       goPackages."github.com/alecthomas/kong"
     ];
   });
+
+  keep-sorted = pkgs.stdenv.mkDerivation (finalAttrs: {
+    pname = "keep-sorted";
+    version = "0.6.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "google";
+      repo = "keep-sorted";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-ROvj7w8YMq6+ntx0SWi+HfN4sO6d7RjKWwlb/9gfz8w=";
+    };
+
+    postPatch = ''
+      substituteInPlace main.go \
+        --replace-fail 'readVersion())' '"v${finalAttrs.version}")'
+    '';
+
+    nativeBuildInputs =
+      let
+        inherit (goPackages) hooks;
+      in
+      [
+        hooks.configureGoVendor
+        hooks.configureGoCache
+        hooks.buildGo
+        hooks.installGo
+      ];
+
+    buildInputs = [
+      goPackages."github.com/Workiva/go-datastructures"
+      goPackages."github.com/google/go-cmp"
+      goPackages."github.com/mattn/go-isatty"
+      goPackages."github.com/rs/zerolog"
+      goPackages."github.com/spf13/pflag"
+      goPackages."gopkg.in/yaml.v3"
+    ];
+
+    nativeInstallCheckInputs = [ pkgs.versionCheckHook ];
+
+    doInstallCheck = true;
+  });
 }
